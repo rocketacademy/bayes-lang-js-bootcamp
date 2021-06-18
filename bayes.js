26ead2debb8764a16e41a250af56f3392cb6fd06
awongh <akira@awongh.com>
2021-06-18 17:53:20 +0800

    refactor for simplicity,move bayes calc for one word around

--- bayes.js
+++ bayes.js
@@ -7,10 +7,6 @@ const Bayes = function () {
     return [...new Set(arr)];
   };
 
-  const log = function (text) {
-    console.log(text);
-  };
-
   const tokenize = function (text) {
     text = text
       .toLowerCase()
@@ -23,10 +19,13 @@ const Bayes = function () {
     return useSet(text);
   };
 
+  const compositeStemLabelKey = (stem, label) => {
+    return stem + "::label:" + label;
+  };
+
   const stemLabelCount = function (stem, label) {
-    let sK = stem + "::label:" + label;
-    let count = parseInt(storage.stems[sK]);
-    if (!count) count = 0;
+    let stemLabelKey = compositeStemLabelKey(stem, label);
+    let count = parseInt(storage.stems[stemLabelKey]) || 0;
     return count;
   };
 
@@ -41,16 +40,15 @@ const Bayes = function () {
   };
 
   const stemTotalCount = function (stem) {
-    let count = parseInt(storage.stemCount[stem]);
-    if (!count) count = 0;
-    console.log("stem total count", stem, count);
+    let count = parseInt(storage.stemCount[stem]) || 0;
     return count;
   };
+
   const docCount = function (label) {
-    let count = parseInt(storage.docCount[label]);
-    if (!count) count = 0;
+    let count = parseInt(storage.docCount[label]) || 0;
     return count;
   };
+
   const docInverseCount = function (label) {
     const labels = storage["labels"];
     let total = 0;
@@ -62,21 +60,23 @@ const Bayes = function () {
   };
 
   const incrementStem = function (stem, label) {
-    let count = parseInt(storage.stemCount[stem]);
-    if (!count) count = 0;
+    // store the absolute word count
+    // the number of times a word appears in *all* the training data
+    let count = parseInt(storage.stemCount[stem]) || 0;
     storage.stemCount[stem] = parseInt(count) + 1;
 
-    let sK = stem + "::label:" + label;
-    count = parseInt(storage.stems[sK]);
-    if (!count) count = 0;
-    storage.stems[sK] = parseInt(count) + 1;
+    // store the word associated with this label
+    // the number of times this word appears in each document / label per training
+
+    // this composite key tracks one unique key for each label - word pair
+    let stemLabelKey = compositeStemLabelKey(stem, label);
+    count = parseInt(storage.stems[stemLabelKey]) || 0;
+    storage.stems[stemLabelKey] = parseInt(count) + 1;
   };
 
   const incrementDocCount = function (label) {
-    let count = parseInt(storage.docCount[label]);
-    if (!count) count = 0;
-    storage.docCount[label] = parseInt(count) + 1;
-    return count + 1;
+    let count = parseInt(storage.docCount[label]) || 0;
+    storage.docCount[label] = count + 1;
   };
 
   const train = function (text, label) {
@@ -92,9 +92,72 @@ const Bayes = function () {
     incrementDocCount(label);
   };
 
-  const guess = function (text) {
+  const labelGuess = (
+    words,
+    totalDocCount,
+    docCounts,
+    docInverseCounts,
+    label
+  ) => {
+    let logSum = 0;
+    let labelProbability = docCounts[label] / totalDocCount;
+
+    // for every word in the input
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+      let result = wordGuess(
+        word,
+        label,
+        docCounts[label],
+        docInverseCounts[label]
+      );
+
+      if (result !== null) {
+        logSum += result;
+      }
+    }
+
+    // change the log back to floating point num
+    let labelResult = 1 / (1 + Math.exp(logSum));
+
+    // return a readable number
+    return labelResult.toFixed(10);
+  };
+
+  const wordGuess = (word, label, docLabelCount, docInverseLabelCount) => {
+    let wordicity = 0;
+    let _stemTotalCount = stemTotalCount(word);
+
+    // if this word isn't in the training data, skip over it
+    if (_stemTotalCount === 0) {
+      return null;
+    }
+
+    let wordProbability = stemLabelCount(word, label) / docLabelCount;
+
+    let wordInverseProbability =
+      stemInverseLabelCount(word, label) / docInverseLabelCount;
+    wordicity = wordProbability / (wordProbability + wordInverseProbability);
+
+    // adjust for rare words
+    // wordicity =
+    //  (1 * 0.5 + _stemTotalCount * wordicity) / (1 + _stemTotalCount);
+
+    // log calculation won't like 0 or 1. adjust these values
+    if (wordicity === 0) {
+      wordicity = 0.01;
+    } else if (wordicity === 1) {
+      wordicity = 0.99;
+    }
+
+    // probability of this word being the label (language )
+    let result = Math.log(1 - wordicity) - Math.log(wordicity);
+    console.log(label + "icity of " + word + ": " + wordicity);
+    return result;
+  };
+
+  const guess = (text) => {
     const words = tokenize(text);
-    let length = words.length;
     const labels = storage["labels"];
     let totalDocCount = 0;
     const docCounts = {};
@@ -112,64 +175,21 @@ const Bayes = function () {
     // for each language
     for (let j = 0; j < labels.length; j++) {
       let label = labels[j];
-      let logSum = 0;
-      let wordicity = 0;
-      labelProbability[label] = docCounts[label] / totalDocCount;
 
-      // for every word in the input
-      for (let i = 0; i < length; i++) {
-        let word = words[i];
-        let _stemTotalCount = stemTotalCount(word);
-
-        // if this word isn't in the training data, skip over it
-        if (_stemTotalCount === 0) {
-          continue;
-        }
-
-        let wordProbability = stemLabelCount(word, label) / docCounts[label];
-
-        let wordInverseProbability =
-          stemInverseLabelCount(word, label) / docInverseCounts[label];
-        wordicity =
-          wordProbability / (wordProbability + wordInverseProbability);
-
-        // adjust for rare words
-        wordicity =
-          (1 * 0.5 + _stemTotalCount * wordicity) / (1 + _stemTotalCount);
-
-        // log calculation won't like 0 or 1. adjust these values
-        if (wordicity === 0) {
-          wordicity = 0.01;
-        } else if (wordicity === 1) {
-          wordicity = 0.99;
-        }
-
-        // add to the logsum the probability of this word being the label (language )
-        logSum += Math.log(1 - wordicity) - Math.log(wordicity);
-        console.log(label + "icity of " + word + ": " + wordicity);
-      }
-
-      // for one language, calculate the total probability given every word we trained over
-      scores[label] = 1 / (1 + Math.exp(logSum));
+      // calculate a score for the group of words
+      scores[label] = labelGuess(
+        words,
+        totalDocCount,
+        docCounts,
+        docInverseCounts,
+        label
+      );
     }
     return scores;
-  };
-
-  const extractWinner = function (scores) {
-    let bestScore = 0;
-    let bestLabel = null;
-    for (let label in scores) {
-      if (scores[label] > bestScore) {
-        bestScore = scores[label];
-        bestLabel = label;
-      }
-    }
-    return { label: bestLabel, score: bestScore };
   };
 
   return {
     train,
     guess,
-    extractWinner,
   };
 };
--- script.js
+++ script.js
@@ -21,9 +21,6 @@ const { train, guess, extractWinner } = Bayes();
 document.querySelector("#test_button").addEventListener("click", function () {
   let text = document.getElementById("test_phrase").value;
   let scores = guess(text);
-  let winner = extractWinner(scores);
-  document.getElementById("test_result").innerHTML = winner.label;
-  document.getElementById("test_probability").innerHTML = winner.score;
 
   let fullResult = Object.keys(scores).map(
     (key, index) => `${key} : ${scores[key]}<br/>`
